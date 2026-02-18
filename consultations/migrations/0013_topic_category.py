5c70c0a8194fd418b1e2ba249c1cc29c7f849201
Ritesh <riteshdhumak95@gmail.com>
2026-02-18 17:02:35 +0530

    migration fixed

--- consultations/migrations/0013_topic_category.py
+++ consultations/migrations/0013_topic_category.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('consultants', '0006_migrate_consultation_fee_data'),
+        ('consultants', '0007_migrate_consultation_fee_data'),
         ('consultations', '0012_alter_topic_consultants'),
     ]
 
